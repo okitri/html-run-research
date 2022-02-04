c79460c4f1b940453e5f87903105805b804d7a70
Oki <oki.tri@runsystem.id>
2022-02-04 13:22:41 +0700

    system_init

---
+++ dist/js/pages/datatables.init.js
@@ -0,0 +1,86 @@
+$(function () {
+  $("#example1").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+  $("#example2").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example2_wrapper .col-md-6:eq(0)');
+  $("#example3").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example3_wrapper .col-md-6:eq(0)');
+    $("#example4").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example4_wrapper .col-md-6:eq(0)');
+    $("#example5").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example5_wrapper .col-md-6:eq(0)');
+    $("#example6").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example6_wrapper .col-md-6:eq(0)');
+    $("#example7").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example7_wrapper .col-md-6:eq(0)');
+    $("#example8").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example8_wrapper .col-md-6:eq(0)');
+    $("#example9").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example9_wrapper .col-md-6:eq(0)');
+    $("#example10").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example10_wrapper .col-md-6:eq(0)');
+    $("#example11").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example11_wrapper .col-md-6:eq(0)');
+    $("#example12").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example12_wrapper .col-md-6:eq(0)');
+    $("#example13").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example13_wrapper .col-md-6:eq(0)');
+    $("#example14").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example14_wrapper .col-md-6:eq(0)');
+    $("#example15").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example15_wrapper .col-md-6:eq(0)');
+    $("#example16").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example16_wrapper .col-md-6:eq(0)');
+    $("#example17").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example17_wrapper .col-md-6:eq(0)');
+    $("#example18").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example18_wrapper .col-md-6:eq(0)');
+    $("#example19").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example19_wrapper .col-md-6:eq(0)');
+    $("#example20").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example20_wrapper .col-md-6:eq(0)');
+    $("#example21").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#example21_wrapper .col-md-6:eq(0)');
+});
